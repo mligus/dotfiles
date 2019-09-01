229fd221c9ffae1f9d4f6d18ce5faa1ec3a01c9b
Max <max.ligus@gmail.com>
2019-09-01 21:19:05 +0300

    move oh-my-zsh to bottom

--- bootstrap.sh
+++ bootstrap.sh
@@ -68,18 +68,6 @@ echo ">>> Installand set up Python enviroment <<<"
 curl -fsSL https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 echo ">>> DONE <<<\n"
 
-echo ">>> Set up Zsh and Oh-My-Zsh <<<"
-sudo apt install -y zsh powerline fonts-powerline
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# Spaceship Prompt
-ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-# Download ZSH config
-curl -fsSL https://raw.githubusercontent.com/mligus/dotfiles/master/zsh/.zshrc -o $HOME/.zshrc
-echo ">>> DONE <<<\n"
-
 echo ">>> Configure NeoVim and install plugins <<<"
 NVIM_CONFIG=$HOME/.config/nvim/
 PYENV_BIN=$HOME/.pyenv/bin/pyenv
@@ -99,4 +87,16 @@ echo ">>> DONE <<<\n"
 
 echo ">>> Cleanup <<<"
 sudo apt autoremove
+echo ">>> DONE <<<\n"
+
+echo ">>> Set up Zsh and Oh-My-Zsh <<<"
+sudo apt install -y zsh powerline fonts-powerline
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Spaceship Prompt
+ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+# Download ZSH config
+curl -fsSL https://raw.githubusercontent.com/mligus/dotfiles/master/zsh/.zshrc -o $HOME/.zshrc
 echo ">>> DONE <<<\n"
